3980cda3cf0b67b1555cd4784ede0f74c9644fe8
Sebastian Velastegui <eliasvelastegui81@gmail.com>
2026-09-03 20:03:08 -0500

    Modelos, seeder y TaskObserver de TaskFlow (patrones Factory y Observer)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        \App\Models\Task::factory()->count(15)->create();
     }
 }
